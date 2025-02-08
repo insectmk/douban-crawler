d9501c0bf0fba0a8caba62bb1c98dc21ed38ac5c
InsectMk <makun_ing@126.com>
2025-02-08 15:17:43 +0800

    增加查询和刷新电影数据的view接口

--- douban_crawler/main/urls.py
+++ douban_crawler/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),# 首页
+    path("refresh_movies", views.refresh_movies, name="refresh_movies"),# 刷新电影数据
+    path("get_movies", views.get_movies, name="get_movies"),# 获取电影数据
 ]
--- douban_crawler/main/views.py
+++ douban_crawler/main/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from . import service
 
-# 第一个视图
+"""
+首页
+"""
 def index(request):
-    return HttpResponse("Hello world!")
+    return HttpResponse(service.get_movies())
+
+"""
+刷新电影
+"""
+def refresh_movies(request):
+    service.refresh_movies() # 爬取数据，并保存到数据库
+    return HttpResponse(service.get_movies())
+
+"""
+获取电影
+"""
+def get_movies(request):
+    return HttpResponse(service.get_movies())
