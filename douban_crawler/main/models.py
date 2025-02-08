496d83728a56846f8f0df2f01930c238977ff1c6
InsectMk <makun_ing@126.com>
2025-02-08 13:54:39 +0800

    增加电影模型（数据表）

--- douban_crawler/main/models.py
+++ douban_crawler/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+"""
+电影
+"""
+class Movies(models.Model):
+    rank = models.IntegerField(default=0)#排名
+    detail_url = models.CharField(max_length=2000)#电影详情链接
+    img_url = models.CharField(max_length=2000)#图片链接
+    name = models.CharField(max_length=200)#影片中文名
+    name_foreign = models.CharField(max_length=200)#影片外国名
+    grade = models.FloatField(default=0.0)#评分
+    num_evaluate = models.BigIntegerField(default=0)#评价数
+    sketch = models.CharField(max_length=2000)#简述
+    other_info = models.CharField(max_length=2000)#相关信息
+    # 打印
+    def __str__(self):
+        return (f"电影: {self.name} (排名: {self.rank}, 评分: {self.grade}, "
+                f"评价数: {self.num_evaluate}, 国外名称: {self.name_foreign})")
