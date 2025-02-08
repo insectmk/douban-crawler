9b3077a6c2a5551c2f6cd07efb24d89ef35b7f82
InsectMk <makun_ing@126.com>
2025-02-08 15:17:14 +0800

    调整模型字段

--- douban_crawler/main/models.py
+++ douban_crawler/main/models.py
@@ -9,11 +9,11 @@ class Movies(models.Model):
     img_url = models.CharField(max_length=2000)#图片链接
     name = models.CharField(max_length=200)#影片中文名
     name_foreign = models.CharField(max_length=200)#影片外国名
-    grade = models.FloatField(default=0.0)#评分
-    num_evaluate = models.BigIntegerField(default=0)#评价数
-    sketch = models.CharField(max_length=2000)#简述
+    rating = models.FloatField(default=0.0)#评分
+    num_judge = models.BigIntegerField(default=0)#评价数
+    inq = models.CharField(max_length=2000)#简述
     other_info = models.CharField(max_length=2000)#相关信息
     # 打印
     def __str__(self):
-        return (f"电影: {self.name} (排名: {self.rank}, 评分: {self.grade}, "
-                f"评价数: {self.num_evaluate}, 国外名称: {self.name_foreign})")
+        return (f"电影: {self.name} (排名: {self.rank}, 评分: {self.rating}, "
+                f"评价数: {self.num_judge}, 国外名称: {self.name_foreign}, 简述: {self.inq})")
