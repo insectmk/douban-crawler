5e0c237a5ba64f26120460e0e09d09d820afdc66
InsectMk <makun_ing@126.com>
2025-02-08 17:21:54 +0800

    增加模版页面

--- douban_crawler/main/urls.py
+++ douban_crawler/main/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 页面
     path("", views.index, name="index"),# 首页
+    path("movie", views.movie, name="movie"),# 电影
+    path("score", views.score, name="score"),# 评分
+    path("word", views.word, name="word"),# 词云
+    # 接口
     path("refresh_movies", views.refresh_movies, name="refresh_movies"),# 刷新电影数据
     path("get_movies", views.get_movies, name="get_movies"),# 获取电影数据
 ]
--- douban_crawler/main/views.py
+++ douban_crawler/main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from . import service
 
 """
-首页
+页面：首页
 """
 def index(request):
     # 获取所有的电影
@@ -14,14 +14,41 @@ def index(request):
     return render(request, "main/index.html", context)
 
 """
-刷新电影
+页面：电影
+"""
+def movie(request):
+    # 给前端传递参数
+    context = {}
+    # 渲染数据
+    return render(request, "main/movie.html", context)
+
+"""
+页面：评分
+"""
+def score(request):
+    # 给前端传递参数
+    context = {}
+    # 渲染数据
+    return render(request, "main/score.html", context)
+
+"""
+页面：词云
+"""
+def word(request):
+    # 给前端传递参数
+    context = {}
+    # 渲染数据
+    return render(request, "main/word.html", context)
+
+"""
+接口：刷新电影
 """
 def refresh_movies(request):
     service.refresh_movies() # 爬取数据，并保存到数据库
     return HttpResponse(service.get_movies())
 
 """
-获取电影
+接口：获取电影
 """
 def get_movies(request):
     return HttpResponse(service.get_movies())
