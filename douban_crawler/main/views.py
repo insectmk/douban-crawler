ef6c7535605e9eb0eb63868e9c5ea3258f7baff6
InsectMk <makun_ing@126.com>
2025-02-08 15:30:52 +0800

    增加首页demo

--- douban_crawler/main/views.py
+++ douban_crawler/main/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from . import service
 
 """
 首页
 """
 def index(request):
-    return HttpResponse(service.get_movies())
+    # 获取所有的电影
+    movies = service.get_movies()
+    # 给前端传递参数
+    context = {"movies": movies}
+    # 渲染数据
+    return render(request, "main/index.html", context)
 
 """
 刷新电影
