d3e063111ca3732d202d55085c3503b94d28b940
InsectMk <makun_ing@126.com>
2025-02-08 15:17:31 +0800

    重新整理爬虫代码，与数据库对接

--- douban_crawler/main/service.py
+++ douban_crawler/main/service.py
@@ -1,83 +1,99 @@
 from bs4 import BeautifulSoup  # 网页解析，获取数据
 import re  # 正则表达式，进行文字匹配`
 import urllib.request, urllib.error  # 制定URL，获取网页数据
-import xlwt  # 进行excel操作
 import time
 import random # 随机数
+from .models import Movies # 电影模型
 
-findLink = re.compile(r'<a href="(.*?)">')  # 创建正则表达式对象，标售规则   影片详情链接的规则
-findImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)
-findTitle = re.compile(r'<span class="title">(.*)</span>')
-findRating = re.compile(r'<span class="rating_num" property="v:average">(.*)</span>')
-findJudge = re.compile(r'<span>(\d*)人评价</span>')
-findInq = re.compile(r'<span class="inq">(.*)</span>')
-findBd = re.compile(r'<p class="">(.*?)</p>', re.S)
+#要爬取的网页链接
+baseurl = "https://movie.douban.com/top250?start="
+# 创建正则表达式对象，标售规则   影片详情链接的规则
+find_detail_url = re.compile(r'<a href="(.*?)">')# 详情链接
+find_img_url = re.compile(r'<img.*src="(.*?)"', re.S)# 图片地址
+find_names = re.compile(r'<span class="title">(.*)</span>')# 电影名称
+find_rating = re.compile(r'<span class="rating_num" property="v:average">(.*)</span>')# 评分
+find_num_judge = re.compile(r'<span>(\d*)人评价</span>') # 评价人数
+find_inq = re.compile(r'<span class="inq">(.*)</span>') # 简述
+find_other_info = re.compile(r'<p class="">(.*?)</p>', re.S) # 相关信息
 
-def main():
-    baseurl = "https://movie.douban.com/top250?start="  #要爬取的网页链接
-    # 1.爬取网页
-    datalist = get_data(baseurl)
-    print(datalist)
-    savepath = "豆瓣电影Top250.xls"    #当前目录新建XLS，存储进去
-    # dbpath = "movie.db"              #当前目录新建数据库，存储进去
-    # 3.保存数据
-    saveData(datalist,savepath)      #2种存储方式可以只选择一种
-    # saveData2DB(datalist,dbpath)
+"""
+获取所有的电影
+"""
+def get_movies():
+    # 获取所有电影数据，并根据排名排序
+    return Movies.objects.all().order_by('rank')
 
-
-
-# 爬取网页
-def get_data(baseurl):
-    datalist = []  #用来存储爬取的网页信息
+"""
+爬取豆瓣top250电影数据
+"""
+def get_data_from_web():
+    movies = []  # 用来存储爬取的网页信息
     for i in range(0, 10):  # 调用获取页面信息的函数，10次
         # 随机延时
         delay = random.uniform(1, 5)
         time.sleep(delay)
         url = baseurl + str(i * 25)
         html = ask_url(url)  # 保存获取到的网页源码
-        # 2.逐一解析数据
+        # 逐一解析数据
         soup = BeautifulSoup(html, "html.parser")
         for item in soup.find_all('div', class_="item"):  # 查找符合要求的字符串
-            data = []  # 保存一部电影所有信息
+            data = {}  # 保存一部电影所有信息
             item = str(item)
-            link = re.findall(findLink, item)[0]  # 通过正则表达式查找
-            data.append(link)
-            imgSrc = re.findall(findImgSrc, item)[0]
-            data.append(imgSrc)
-            titles = re.findall(findTitle, item)
-            if (len(titles) == 2):
-                ctitle = titles[0]
-                data.append(ctitle)
-                otitle = titles[1].replace("/", "")  #消除转义字符
-                data.append(otitle)
+            data['detail_url'] = re.findall(find_detail_url, item)[0]  # 详情链接
+            data['img_url'] = re.findall(find_img_url, item)[0]  # 图片链接
+            names = re.findall(find_names, item)  # 电影名称
+            if len(names) == 2:
+                data['name'] = names[0]  # 电影名
+                data['name_foreign'] = names[1].replace("/", "")  # 外国电影名
             else:
-                data.append(titles[0])
-                data.append(' ')
-            rating = re.findall(findRating, item)[0]
-            data.append(rating)
-            judgeNum = re.findall(findJudge, item)[0]
-            data.append(judgeNum)
-            inq = re.findall(findInq, item)
+                data['name'] = names[0]  # 电影名
+                data['name_foreign'] = ''  # 外国电影名，为空
+            data['rating'] = float(re.findall(find_rating, item)[0])  # 评分
+            data['num_judge'] = int(re.findall(find_num_judge, item)[0])  # 评价数
+            inq = re.findall(find_inq, item)  # 简述
             if len(inq) != 0:
-                inq = inq[0].replace("。", "")
-                data.append(inq)
+                data['inq'] = inq[0].replace("。", "")  # 简述
             else:
-                data.append(" ")
-            bd = re.findall(findBd, item)[0]
-            bd = re.sub('<br(\\s+)?/>(\\s+)?', "", bd)
-            bd = re.sub('/', "", bd)
-            data.append(bd.strip())
-            datalist.append(data)
+                data['inq'] = ""  # 简述，为空
+            other_info = re.findall(find_other_info, item)[0]
+            other_info = re.sub('<br(\\s+)?/>(\\s+)?', "", other_info)
+            other_info = re.sub('/', "", other_info)
+            data['other_info'] = other_info.strip()  # 相关信息
+            movies.append(data)  # 加入到电影集合中
+    return movies
 
-    return datalist
+"""
+将爬取的数据保存到数据库
+"""
+def refresh_movies():
+    # 删除旧的数据
+    Movies.objects.all().delete()
+    # 获取爬取的数据
+    movies_data = get_data_from_web()
+    # 遍历数据并保存到数据库
+    for index, movie_data in enumerate(movies_data):
+        movie = Movies(
+            rank=index + 1,  # 假设排名是顺序递增的
+            detail_url=movie_data['detail_url'],
+            img_url=movie_data['img_url'],
+            name=movie_data['name'],
+            name_foreign=movie_data['name_foreign'],
+            rating=movie_data['rating'],
+            num_judge=movie_data['num_judge'],
+            inq=movie_data['inq'],
+            other_info=movie_data['other_info']
+        )
+        movie.save()  # 保存到数据库
+    return len(movies_data)
 
-
-# 得到指定一个URL的网页内容
+"""
+得到指定一个URL的网页内容
+"""
 def ask_url(url):
-    head = {  # 模拟浏览器头部信息，向豆瓣服务器发送消息
+    # 模拟浏览器头部信息，向豆瓣服务器发送消息
+    head = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
     }
-    # 用户代理，表示告诉豆瓣服务器，我们是什么类型的机器、浏览器（本质上是告诉浏览器，我们可以接收什么水平的文件内容）
 
     request = urllib.request.Request(url, headers=head, )
     html = ""
@@ -90,76 +106,3 @@ def ask_url(url):
         if hasattr(e, "reason"):
             print(e.reason)
     return html
-
-
-# 保存数据到表格
-def saveData(datalist,savepath):
-    print("save.......")
-    book = xlwt.Workbook(encoding="utf-8",style_compression=0) #创建workbook对象
-    sheet = book.add_sheet('豆瓣电影Top250', cell_overwrite_ok=True) #创建工作表
-    col = ("电影详情链接","图片链接","影片中文名","影片外国名","评分","评价数","概况","相关信息")
-    for i in range(0,8):
-        sheet.write(0,i,col[i])  #列名
-    for i in range(0,250):
-        # print("第%d条" %(i+1))       #输出语句，用来测试
-        data = datalist[i]
-        for j in range(0,8):
-            sheet.write(i+1,j,data[j])  #数据
-    book.save(savepath) #保存
-
-# def saveData2DB(datalist,dbpath):
-#     init_db(dbpath)
-#     conn = sqlite3.connect(dbpath)
-#     cur = conn.cursor()
-#     for data in datalist:
-#             for index in range(len(data)):
-#                 if index == 4 or index == 5:
-#                     continue
-#                 data[index] = '"'+data[index]+'"'
-#             sql = '''
-#                     insert into movie250(
-#                     info_link,pic_link,cname,ename,score,rated,instroduction,info)
-#                     values (%s)'''%",".join(data)
-#             # print(sql)     #输出查询语句，用来测试
-#             cur.execute(sql)
-#             conn.commit()
-#     cur.close
-#     conn.close()
-
-
-# def init_db(dbpath):
-#     sql = '''
-#         create table movie250(
-#         id integer  primary  key autoincrement,
-#         info_link text,
-#         pic_link text,
-#         cname varchar,
-#         ename varchar ,
-#         score numeric,
-#         rated numeric,
-#         instroduction text,
-#         info text
-#         )
-#
-#
-#     '''  #创建数据表
-#     conn = sqlite3.connect(dbpath)
-#     cursor = conn.cursor()
-#     cursor.execute(sql)
-#     conn.commit()
-#     conn.close()
-
-# 保存数据到数据库
-
-
-
-
-
-
-
-if __name__ == "__main__":  # 当程序执行时
-    # 调用函数
-     main()
-    # init_db("movietest.db")
-     print("爬取完毕！")
-
